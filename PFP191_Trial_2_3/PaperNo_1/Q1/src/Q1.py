20ad4f2bafc34e3e892f6495e6a9705158d46d17
buithetuan <btt23603@gmail.com>
2025-11-03 09:04:20 +0700

    udpate source

---
+++ PFP191_Trial_2_3/PaperNo_1/Q1/src/Q1.py
@@ -0,0 +1,49 @@
+class Main:
+    
+    #====================f1====================
+    def f1(self, inputString):
+        # ===YOU CAN EDIT OR EVEN ADD NEW FUNCTIONS IN THE FOLLOWING PART========
+        movement = inputString.split(', ')
+        for move in movement:
+            print(move)
+        # end def
+
+
+    #====================f2====================
+    def f2(self, inputString):
+        # ===YOU CAN EDIT OR EVEN ADD NEW FUNCTIONS IN THE FOLLOWING PART========
+        x,y = 0,0
+        movement = inputString.split(', ')
+        for move in movement:
+            part = move.split(' ')
+            direction = part[0]
+            step = int(part[1])
+            if direction == 'UP':
+                y += step
+            elif direction == 'DOWN':
+                y -= step
+            elif direction == 'LEFT':
+                x -= step
+            elif direction == 'RIGHT':
+                x += step
+        
+        distance = (x**2 + y**2) ** 0.5
+        print(round(distance))
+     
+#================DO NOT CHANGE THE CODE BELOW===============================
+    def main(self):
+        print(" 1. Test f1 (2 mark)")
+        print(" 2. Test f2 (1 mark)")
+        print(" Your selection (1 -> 2): ")
+        choice = int(input())
+        inputString = input()
+        print("OUTPUT")
+        if choice == 1:
+            self.f1(inputString)
+        elif choice == 2:
+            self.f2(inputString)
+        else:
+            print("Wrong select")
+        print("FINISH")
+main = Main()
+main.main()
---
+++ PFP191_Trial_2_3/PaperNo_1/Q2/src/Q2.py
@@ -0,0 +1,16 @@
+class Main:
+
+    #====================GuessNumber function====================
+    def GuessNumber(self, number):
+        # ===YOU CAN EDIT OR EVEN ADD NEW FUNCTIONS IN THE FOLLOWING PART========
+        
+        # end def
+
+#==================DO NOT CHANGE THE CODE BELOW=====================
+    def main(self):
+        number = input("Enter a range for guessed number: ")
+        print("OUTPUT")
+        self.GuessNumber(number)
+        print("FINISH")
+main = Main()
+main.main()
